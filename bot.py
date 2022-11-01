d74865ba10c9de1c03c4c2a744069371d1fd4b32
Ayush Pandit <ayushtripathi.india@gmail.com>
2022-11-02 01:46:10 +0530

    adding the initial code of bot

---
+++ bot.py
@@ -0,0 +1,76 @@
+from telegram.ext import *
+import api
+from datetime import datetime
+
+# import message_responses as r
+
+import weather as wet
+
+print("Bot started...")
+
+def start_command(update, context):
+    update.message.reply_text(
+        """
+        Hello! welcome to our Bot.
+
+Type /help to see the command details""")
+
+def help_command(update, context):
+    update.message.reply_text(
+    """
+    Command details:-
+
+/help to see what can we do for you.
+/team to see the details of developers.
+
+You can write the city name to know the weather of that city.
+Note:- Please write the correct spelling of the city to avoid error.
+    """
+    )
+
+
+now = datetime.now()
+date_time = now.strftime(" Today is %d.%m.20%y and the time is %H:%M")
+str(date_time)
+
+def do_command(update, context):
+    text = str(update.message.text).lower()
+    response = wet.weather_response(text)
+    
+    update.message.reply_text(date_time)
+    update.message.reply_text(response)
+    update.message.reply_text("Thanks for using our Service :)")
+
+
+def details_command(update, context):
+    update.message.reply_text(
+    """
+    Our Team:-
+
+    Rishabh Srivastava
+    dear @ Priyambada
+    Ayush Tripathi @the_ayushtripathi
+    """
+    )
+
+def stop_command(update, context):
+    update.message.reply_text("Thanks for using our services, Regards our team.")
+
+
+def main():
+    updater= Updater(api.telegram_key, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(CommandHandler("team", details_command))
+    dp.add_handler(CommandHandler("stop", stop_command))
+    # dp.add_handler(CommandHandler("weather", weather_command))
+    # dp.add_handler(MessageHandler(Filters.text, weather_command))
+    dp.add_handler(MessageHandler(Filters.text, do_command))
+
+    updater.start_polling(3)
+    updater.idle()
+
+main()
+
---
+++ weather.py
@@ -0,0 +1,27 @@
+import api
+import requests
+
+
+#weather data retrival section
+
+key = api.weather_key
+def weather_response(city_name):
+    api_link = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+key
+    json_data = requests.get(api_link).json()
+    # print(json_data)
+    weather_condition = json_data['weather'][0]['main']
+    temp = str(int(json_data['main']['temp']-273.15))
+
+    final_result = "and the present weather condition of "+city_name+" is "+weather_condition+" and the temprature is "+temp+" deg. C"
+    return final_result
+
+# print(weather_response('allahabad'))
+
+
+#message processing part
+
+
+
+#telebot section
+
+
